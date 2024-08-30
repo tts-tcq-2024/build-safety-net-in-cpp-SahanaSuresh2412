105cab3d5944d72deb1cddd361e3b242694ae849
SahanaSuresh2412 <sahanasuresh.2412@gmail.com>
2024-08-30 19:23:01 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,6 +4,14 @@
 #include <string>
 
 std::string generateSoundex(const std::string& name);
-map<char, int> Soundex;
+map<char, int> soundexMap ={
+        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
+        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'},
+        {'S', '2'}, {'X', '2'}, {'Z', '2'},
+        {'D', '3'}, {'T', '3'},
+        {'L', '4'},
+        {'M', '5'}, {'N', '5'},
+        {'R', '6'}
+}
 
 #endif // SOUNDEX_H
