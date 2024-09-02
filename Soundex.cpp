93c7516b3d448e6a113ad99e47b0ba01af97f31d
SahanaSuresh2412 <sahanasuresh.2412@gmail.com>
2024-09-02 17:32:02 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -11,15 +11,6 @@ char getSoundexCode(char c, const std::map<char, char>& soundexMap) {
 }
 
 std::string generateSoundex(const std::string& name) {
-     std::map<char, char> soundexMap = {
-        {'A', '0'}, {'E', '0'}, {'I', '0'}, {'O', '0'}, {'U', '0'},
-        {'H', '0'}, {'W', '0'}, {'Y', '0'}, {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
-        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
-        {'D', '3'}, {'T', '3'},
-        {'L', '4'},
-        {'M', '5'}, {'N', '5'},
-        {'R', '6'}
-    };
     if (name.empty()) return "";
 
     std::string soundex(1, toupper(name[0]));
