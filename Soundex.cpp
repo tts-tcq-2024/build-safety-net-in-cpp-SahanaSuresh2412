8ffc9eb4ddd541c1de5c0e005da6635093208e43
SahanaSuresh2412 <sahanasuresh.2412@gmail.com>
2024-09-05 01:03:28 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,13 +1,13 @@
-// Function to get the Soundex code for a character using a local map
-char getSoundexCode(char c, const std::map<char, char>& soundexMap) {
-    c = toupper(c);
-    auto it = soundexMap.find(c);
-    return (it != soundexMap.end()) ? it->second : '0';
-}
-
-std::string generateSoundex(const std::string& name) {
-    // Local Soundex map for encoding characters
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <map>
+ 
+using namespace std;
+ 
+char getSoundexCode(char c) {
     std::map<char, char> soundexMap = {
+ 
         {'A', '0'}, {'E', '0'}, {'I', '0'}, {'O', '0'}, {'U', '0'},
         {'H', '0'}, {'W', '0'}, {'Y', '0'}, {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
         {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
@@ -16,21 +16,47 @@ std::string generateSoundex(const std::string& name) {
         {'M', '5'}, {'N', '5'},
         {'R', '6'}
     };
-
+    
+    auto it = soundexMap.find(toupper(c));
+    return (it != soundexMap.end()) ? it->second : '0';
+}
+ 
+ 
+std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
-
+ 
     std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0], soundexMap);
-
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i], soundexMap);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
+    char prevCode = getSoundexCode(name[0]);
+    for (size_t i = 1; i <= name.length() && soundex.length() < 4; ++i) {
+        char code = getSoundexCode(toupper(name[i]));
+        if(code == prevCode){
+            continue;
         }
+        if ((name[i - 1] == 'H' || name[i - 1] == 'W' || name[i - 1] == 'Y') && code != '0' && code != prevCode) 
+        {
+            soundex += code;
+        }
+        else if ((name[i - 1] != 'A' && name[i - 1] != 'E' && name[i - 1] != 'I' && name[i - 1] != 'O' && name[i - 1] != 'U') && code != '0')
+        {
+            soundex += code;
+        }
+        prevCode = code;
     }
-
+ 
     soundex.resize(4, '0');
-
+    
+ 
     return soundex;
+}
+ 
+ 
+int main() {
+    std::string name;
+    std::cout << "Enter a name: ";
+    std::cin >> name;
+
+    std::string soundexCode = generateSoundex(name);
+    std::cout << "Soundex code for " << name << " is " << soundexCode << std::endl;
+
+    return 0;
 }
