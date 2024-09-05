bdf6f694d1c6fa3d8edf4de9e469d175af9d07d2
SahanaSuresh2412 <sahanasuresh.2412@gmail.com>
2024-09-05 12:37:10 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "Soundex.h"
+#include "Soundex.cpp"
 
 
 TEST(SoundexTest, HandlesEmptyString) {
@@ -10,3 +10,31 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("A"), "0");
+}
+
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("B"), "1");
+}
+
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("B"), "1");
+}
+
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("C"), "2");
+}
+
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("D"), "3");
+}
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("L"), "4");
+}
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("M"), "5");
+}
+TEST(SoundexTest, getSoundexCode){
+    EXPECT_EQ(getSoundexCode("R"), "6");
+}
